a53a8ad276d1c7f311d19474026d92b5271925f8
diozenio <d.dionisiobsl@gmail.com>
2023-04-13 18:25:56 -0300

    Export all components

---
+++ src/ui/components/index.ts
@@ -0,0 +1,7 @@
+import Cover from "@components/Cover";
+import Description from "@components/Description";
+import Title from "@components/Title";
+import TextInput from "@components/Form/TextInput";
+import Button from "@components/Form/Button";
+
+export { Cover, Description, Title, TextInput, Button };
