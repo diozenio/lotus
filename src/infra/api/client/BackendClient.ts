d38ff5ea1722f078076f43e76ebbca642fc2e4f0
diozenio <d.dionisiobsl@gmail.com>
2023-05-02 17:16:53 -0300

    Add error interception to Backend client

--- src/infra/api/client/BackendClient.ts
+++ src/infra/api/client/BackendClient.ts
@@ -7,3 +7,16 @@ export const BackendClient = axios.create({
   },
   timeout: 10000,
 });
+
+BackendClient.interceptors.response.use(
+  (response) => {
+    if (response.data.status === "error") {
+      return Promise.reject(new Error(String(response.data.error)));
+    }
+
+    return response;
+  },
+  (error) => {
+    return Promise.reject(new Error(error.response.data.error));
+  }
+);
