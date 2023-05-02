54979e357a5f38d6914fefaf5c1f160f6a37d78a
diozenio <d.dionisiobsl@gmail.com>
2023-05-02 17:19:36 -0300

    Add Auth provider

---
+++ src/ui/contexts/auth/AuthCTX.ts
@@ -0,0 +1,24 @@
+import Credentials from "@models/auth/Credentials";
+import User from "@models/auth/User";
+import React, { createContext } from "react";
+
+interface Props {
+  user: User | undefined | null;
+  signUp: (user: User) => Promise<void>;
+  signIn: (credentials: Credentials) => Promise<void>;
+  findUserById: (userID: string) => Promise<void>;
+  logout: () => Promise<void>;
+  panic(err: unknown): Promise<boolean>;
+}
+
+export const AuthCTX = createContext<Props>({} as Props);
+
+export const useAuth = () => {
+  const context = React.useContext(AuthCTX);
+
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+
+  return context;
+};
---
+++ src/ui/contexts/auth/AuthProvider.tsx
@@ -0,0 +1,118 @@
+import { PropsWithChildren, useCallback, useState } from "react";
+import { AuthCTX } from "./AuthCTX";
+import User from "@models/auth/User";
+import Credentials from "@models/auth/Credentials";
+import AuthService from "@services/AuthService";
+import AuthAPI from "@api/auth";
+import CacheService from "@services/CacheService";
+
+function AuthProvider({ children }: PropsWithChildren) {
+  const [user, setUser] = useState<User | null>();
+
+  let cachedUser = CacheService.getAuthResponse();
+
+  const authService = new AuthService(new AuthAPI());
+
+  const signIn = useCallback(async (credentials: Credentials) => {
+    try {
+      const response = await authService.login(credentials);
+      setUser(response);
+    } catch (error) {
+      panic(error);
+    }
+  }, []);
+
+  const signUp = useCallback(async (user: User) => {
+    try {
+      const response = await authService.register(user);
+      setUser(response);
+    } catch (error) {
+      panic(error);
+    }
+  }, []);
+
+  const findUserById = useCallback(async (userId: string) => {
+    try {
+      const response = await authService.findUserById(userId);
+      setUser(response);
+    } catch (error) {
+      panic(error);
+    }
+  }, []);
+
+  const logout = useCallback(async () => {
+    try {
+      await authService.logout(user?.id!);
+      setUser(null);
+    } catch (error) {
+      panic(error);
+    }
+  }, []);
+
+  const getCachedUser = useCallback(async () => {
+    try {
+      if (cachedUser) {
+        await authService.configureAuthorization(cachedUser);
+        const response = await authService.findUserById(
+          cachedUser.refreshToken?.id!
+        );
+        setUser(response);
+      }
+    } catch (error) {
+      panic(error);
+    }
+  }, []);
+
+  const useErrorHandler = useCallback(
+    async (error: unknown): Promise<[string, boolean]> => {
+      if (error instanceof Error) {
+        switch (error.message) {
+          case "Invalid Token":
+            const shouldLogout = await authService.updateToken(cachedUser!);
+            if (shouldLogout)
+              return ["Sua sessão expirou, logue novamente!", shouldLogout];
+            cachedUser = CacheService.getAuthResponse();
+            getCachedUser();
+            return ["", shouldLogout];
+
+          case "User already exists":
+            return [
+              "E-mail já cadastrado. Tente fazer login ou use outro e-mail.",
+              false,
+            ];
+          case "Unauthorized":
+            return ["Sua sessão expirou, logue novamente!", true];
+          default:
+            return [error.message, true];
+        }
+      }
+      return ["Unexpected error", true];
+    },
+    []
+  );
+
+  const panic = useCallback(async (error: unknown): Promise<boolean> => {
+    const [message, shouldLogout] = await useErrorHandler(error);
+    if (shouldLogout) {
+      logout();
+    }
+    if (import.meta.env.DEV && message) {
+      console.error("ERROR:", message);
+    }
+    return shouldLogout;
+  }, []);
+
+  if (!user && cachedUser) {
+    getCachedUser();
+  }
+
+  return (
+    <AuthCTX.Provider
+      value={{ findUserById, logout, signIn, signUp, user, panic }}
+    >
+      {children}
+    </AuthCTX.Provider>
+  );
+}
+
+export default AuthProvider;
